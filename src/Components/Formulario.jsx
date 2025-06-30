9d4d332512636b03fc57e7a7809d366f468ade3c
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-06-30 22:55:38 +0200

    crearemos un custom hook para separar logica de vista

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import { useState } from 'react';
 
 export const Formulario = () => {
+
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+    username: ''
+  })
+
+  const { email, password, username } = formState
+
+  const handleForm = ({ target }) => {
+    const { name, value } = target
+    setFormState({
+      ...formState,
+      [name]: value
+    })
+
+  }
+
+  const onsubmitForm = (event) => {
+    event.preventDefault()
+    console.log(formState)
+  }
+
   return (
-    <>
-    </>
+    <form onSubmit={onsubmitForm}>
+
+      <div className="form-group">
+        <label htmlFor="email">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          name="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={handleForm}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handleForm}
+
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="username">User Name</label>
+        <input
+          type="text"
+          className="form-control"
+          name="username"
+          placeholder="Enter username"
+          value={username}
+          onChange={handleForm}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">Submit</button>
+
+    </form>
   )
 }
